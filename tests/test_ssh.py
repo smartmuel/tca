4c6109e15e6b2f21eab04fd7c5e27f6ec16aa2f0
Shmuel Gibhin Zudik <ShmuelG@radware.com>
2021-10-02 16:35:29 +0300

    update

--- tests/test_ssh.py
+++ tests/test_ssh.py
@@ -1,5 +1,4 @@
 from tca import *
-import pytest
 
 Tools.exc = True
 user, password = "smartmuel1", "IXSZW5caiAQrQQ"
@@ -8,7 +7,8 @@ NO_PING = ["172.16.255.254", user, password]
 SSH_DISABLED = ["google.com", user, password]
 SSH_WRONG_LOGIN = ["tty.sdf.org", "wrong", "login"]
 
-class TestSSH():
+
+class TestSSH:
     def test_ssh_enabled_no_ping(self):
         try:
             ssh = SSH(NO_PING[0], NO_PING[1], NO_PING[2])
@@ -20,22 +20,22 @@ class TestSSH():
     def test_ssh_enabled_no_ping_cm(self):
         try:
             with CM.ssh(NO_PING[0], NO_PING[1], NO_PING[2]) as ssh:
-                pass
+                ssh.command(" ")
             raise
         except Exc.NoPingError:
             pass
 
     def test_ssh_enabled_ping(self):
-        ssh = SSH(SSH_ENABLED[0],SSH_ENABLED[1],SSH_ENABLED[2])
+        ssh = SSH(SSH_ENABLED[0], SSH_ENABLED[1], SSH_ENABLED[2])
         ssh.close()
 
     def test_ssh_enabled_ping_cm(self):
-        with CM.ssh(SSH_ENABLED[0],SSH_ENABLED[1],SSH_ENABLED[2]) as ssh:
-            pass
+        with CM.ssh(SSH_ENABLED[0], SSH_ENABLED[1], SSH_ENABLED[2]) as ssh:
+            ssh.command(" ")
 
     def test_ssh_disabled(self):
         try:
-            ssh = SSH(SSH_DISABLED[0],SSH_DISABLED[1],SSH_DISABLED[2])
+            ssh = SSH(SSH_DISABLED[0], SSH_DISABLED[1], SSH_DISABLED[2])
             ssh.close()
             raise
         except Exc.SSHError:
@@ -43,15 +43,15 @@ class TestSSH():
 
     def test_ssh_disabled_cm(self):
         try:
-            with SSH(SSH_DISABLED[0],SSH_DISABLED[1],SSH_DISABLED[2]) as ssh:
-                pass
+            with SSH(SSH_DISABLED[0], SSH_DISABLED[1], SSH_DISABLED[2]) as ssh:
+                ssh.command(" ")
             raise
         except Exc.SSHError:
             pass
 
     def test_ssh_wrong_login(self):
         try:
-            ssh = SSH(SSH_WRONG_LOGIN[0],SSH_WRONG_LOGIN[1],SSH_WRONG_LOGIN[2])
+            ssh = SSH(SSH_WRONG_LOGIN[0], SSH_WRONG_LOGIN[1], SSH_WRONG_LOGIN[2])
             ssh.close()
             raise
         except Exc.SSHError:
@@ -59,26 +59,17 @@ class TestSSH():
 
     def test_ssh_wrong_login_cm(self):
         try:
-            with SSH(SSH_WRONG_LOGIN[0],SSH_WRONG_LOGIN[1],SSH_WRONG_LOGIN[2]) as ssh:
-                pass
+            with SSH(SSH_WRONG_LOGIN[0], SSH_WRONG_LOGIN[1], SSH_WRONG_LOGIN[2]) as ssh:
+                ssh.command(" ")
             raise
         except Exc.SSHError:
             pass
 
     def test_ssh_command(self):
         ssh = SSH(SSH_ENABLED[0], SSH_ENABLED[1], SSH_ENABLED[2])
-        ssh.command(" ",line=0)
-        ssh.close()
-
-    def test_ssh_command_cm(self):
-        with CM.ssh(SSH_ENABLED[0],SSH_ENABLED[1],SSH_ENABLED[2]) as ssh:
-            ssh.command(" ",line=0)
-
-    def test_ssh_command_shell(self):
-        ssh = SSH(SSH_ENABLED[0], SSH_ENABLED[1], SSH_ENABLED[2],shell=True,shell_key="")
         ssh.command(" ")
         ssh.close()
 
-    def test_ssh_command_shell_cm(self):
-        with CM.ssh(SSH_ENABLED[0],SSH_ENABLED[1],SSH_ENABLED[2],shell=True,shell_key="") as ssh:
+    def test_ssh_command_cm(self):
+        with CM.ssh(SSH_ENABLED[0], SSH_ENABLED[1], SSH_ENABLED[2]) as ssh:
             ssh.command(" ")
